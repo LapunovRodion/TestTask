7ed77aff103f49e57b6bc7109b6189060173dfbc
threedots65 <98290096+threedots65@users.noreply.github.com>
2024-12-06 17:40:26 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,41 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            // Автора, который написал книгу с самым длинным названием ( в случае если таких авторов окажется несколько, необходимо вернуть автора с наименьшим Id)
+            var longestBook = await _context.Books.OrderByDescending(b => b.Title.Length).ThenBy(b => b.Id).FirstOrDefaultAsync();
+            return longestBook != null ? await _context.Authors.FirstOrDefaultAsync(a => a.Id == longestBook.AuthorId) : null;
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            // Авторов, написавших четное количество книг, изданных после 2015 года
+            var authorBookCounts = await _context.Books.Where(b => b.PublishDate.Year > 2015)
+                                                       .GroupBy(b => b.AuthorId)
+                                                       .Select(g => new { AuthorId = g.Key, Count = g.Count() })
+                                                       .Where(g => g.Count % 2 == 0)
+                                                       .Select(g => g.AuthorId)
+                                                       .ToListAsync();
+
+            return await _context.Authors.Where(a => authorBookCounts.Contains(a.Id)).ToListAsync();
+        }
+    }
+}
+
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,31 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            // Книгу с наибольшей стоимостью опубликованного тиража
+            return await _context.Books.OrderByDescending(b => b.Price * b.QuantityPublished).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            // Книги, в названии которой содержится "Red" и которые опубликованы после выхода альбома "Carolus Rex" группы Sabaton
+            return await _context.Books.Where(b => b.Title.Contains("Red") && b.PublishDate.Year > 2012).ToListAsync();
+        }
+    }
+}
